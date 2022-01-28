37ca3d733345573a77ffe22cef53093dbd9c5295
Raihan Adam <hallwack.id@gmail.com>
2022-01-28 18:23:15 +0700

    Add Redux; Making initState and action

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./redux/reducer";
+
+const store = createStore(rootReducer);
+
+// udah bikin store, reducer, sama initialState
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/redux/reducer.js
@@ -0,0 +1,28 @@
+import initialState from "./initialState";
+import actionType from "./actionType";
+
+export const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionType.HANDLE_ANSWER_QUESTION:
+      return {
+        ...state,
+      };
+    case actionType.HANDLE_NAME:
+      return {
+        ...state,
+        name: action.payload,
+      };
+    case actionType.HANDLE_SCORE:
+      return {
+        ...state,
+        score: action.payload,
+      };
+    case actionType.HANDLE_QUIZ_STATE:
+      return {
+        ...state,
+        quizState: action.payload,
+      };
+    default:
+      break;
+  }
+};
